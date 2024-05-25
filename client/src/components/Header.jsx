8ee6bca9cbe10469b4a5ecd1c8f54fe501d0d903
Goitom <goitommahtsentu@gmail.com>
2024-05-26 01:52:12 +0200

    create and run backend

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -30,7 +30,7 @@ const Header = () => {
                     <FaMoon/>
                </Button>
                <Link to='/sign-in'>
-                   <Button gradientDuoTone='purpleToBlue' pill>
+                   <Button gradientDuoTone='purpleToBlue' outline>
                          Sign In
                    </Button>
                </Link>
