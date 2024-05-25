baa22a1ca77dbea3230fe248b1541bcb8c2e97ce
Goitom <goitommahtsentu@gmail.com>
2024-05-26 01:05:54 +0200

    create the header section

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,10 +1,14 @@
+import * as flowbite from "flowbite-react/tailwind";
+
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
+    flowbite.content(),
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [flowbite.plugin(),],
+
 }
