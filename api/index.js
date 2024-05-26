b5adaa8985c0e350094e19d8df5081cd6d9f2609
Goitom <goitommahtsentu@gmail.com>
2024-05-26 23:46:39 +0200

    create database

--- api/index.js
+++ api/index.js
@@ -1,7 +1,15 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config()
 const app = express();
 
 
-app.listen(3000,()=>{
+mongoose.connect(process.env.MONGO_DB).then(()=>{
+        console.log('MongoDB Connected!');
+}).catch(err=>{
+    console.log(err);
+})
+app.listen(8000,()=>{
     console.log("Server running on port 3000");
 })
