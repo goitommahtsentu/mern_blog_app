a7801ed955e453ecfc439151bea7b49e0b9ad369
Goitom <goitommahtsentu@gmail.com>
2024-06-08 02:52:45 +0200

    add functionality to the signup page

--- api/index.js
+++ api/index.js
@@ -4,25 +4,25 @@ import bodyParser from 'body-parser';
 import dotenv from "dotenv";
 import userRoute from "./routes/user.route.js";
 import authRoute from "./routes/auth.route.js";
+import cors from 'cors';
 
 dotenv.config()
 const app = express();
 
 
 app.use(express.json());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
 mongoose.connect(process.env.MONGO_DB).then(() => {
     console.log('MongoDB Connected!');
 }).catch(err => {
     console.log(err);
 })
-app.use("/api/user", userRoute);
-app.use("/api/auth", authRoute);
-app.listen(8000, () => {
+
+app.listen(process.env.PORT, () => {
     console.log("Server running on port 8000");
 })
-
+app.use("/api/user", userRoute);
+app.use("/api/auth", authRoute);
 app.use((err, req, res, next) => {
     const statusCode = err.statusCode || 500;
     const message = err.message || "Internal server error";
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,8 +1,46 @@
-import React from 'react';
-import {Link} from "react-router-dom";
-import {Button, Label, TextInput} from "flowbite-react";
+import React, {useState} from 'react';
+import {Link, useNavigate} from "react-router-dom";
+import {Alert, Button, Label, Spinner, TextInput} from "flowbite-react";
 
 const SignUp = () => {
+    const [formData, setFormData] = useState({
+        username: '', email: '', password: '' });
+    const [errorMessage, setErrorMessage] = useState(null);
+    const [loading,setLoading]=useState(false)
+    const navigate=useNavigate()
+    const handleChange = (e) => {
+        e.preventDefault();
+       setFormData({...formData, [e.target.id]: e.target.value.trim()});
+
+    }
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        if(!formData.username || !formData.email || !formData.password ) {
+            return setErrorMessage('please fill out all fields')
+        }
+
+        try {
+            setLoading(true)
+            setErrorMessage(null)
+            const res = await fetch('/api/auth/sign-up', {
+                method: "POST",
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify(formData),
+            });
+
+            const data = await res.json();
+            if (data.success===false) {
+                return setErrorMessage(data.message);
+            }
+            setLoading(false)
+            if (res.ok){
+                navigate('/sign-in')
+            }
+        } catch (error) {
+            setErrorMessage(error.message)
+            setLoading(false)
+        }
+    };
     return (
         <div className="min-h-screen mt-20">
            <div className="flex p-3 max-w-3xl mx-auto flex-col md:flex-row gap-5">
@@ -18,13 +56,14 @@ const SignUp = () => {
                </div>
              {/*right side*/}
                <div className="flex-1">
-                   <form className='flex flex-col gap-4'>
+                   <form onSubmit={handleSubmit} className='flex flex-col gap-4' >
                        <div>
                            <Label value='your userame'/>
                            <TextInput
                                type='text'
                                placeholder='username'
                                id='username'
+                               onChange={handleChange}
                            />
                        </div>
                        <div>
@@ -33,6 +72,7 @@ const SignUp = () => {
                                type='email'
                                placeholder='name@company.com'
                                id='email'
+                               onChange={handleChange}
                            />
                        </div>
                        <div>
@@ -41,16 +81,28 @@ const SignUp = () => {
                                type='password'
                                placeholder='password'
                                id='password'
+                               onChange={handleChange}
                            />
                        </div>
-                       <Button gradientDuoTone='purpleToPink' type='submit'>
-                       Sign up
+                       <Button gradientDuoTone='purpleToPink' type='submit' disabled={loading}>
+                           {loading ?(
+                               <>
+                                   <Spinner size='sm'/>
+                                   <span className='pl-3'>Loading</span>
+                               </>
+
+                           ) : "Sign Up"}
                        </Button>
                    </form>
                    <div className="flex gap-2 text-sm mt-5">
                        <span>haven an account?</span>
                        <Link to='/sign-in' className='text-blue-500'>Sign In</Link>
                    </div>
+                   {errorMessage && (
+                       <Alert className='mt-5' color='failure'>
+                           {errorMessage}
+                       </Alert>
+                   )}
                </div>
            </div>
         </div>
