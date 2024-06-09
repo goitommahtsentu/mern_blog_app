c9ea6331b335ca589d82aee7a80ec49292e5f574
Goitom <goitommahtsentu@gmail.com>
2024-06-09 17:15:05 +0200

    adding redux toolkit with persist

---
+++ client/src/redux/store.js
@@ -0,0 +1,31 @@
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import userReducer from './user/userSlice.js';
+import { persistReducer, persistStore } from 'redux-persist';
+import storage from 'redux-persist/lib/storage'; // defaults to localStorage for web
+
+// Combine all the reducers
+const rootReducer = combineReducers({
+    user: userReducer,
+});
+
+// Configuration for redux-persist
+const persistConfig = {
+    key: 'root',
+    storage,
+    version: 1,
+};
+
+// Creating the persisted reducer
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+// Configure the store
+export const store = configureStore({
+    reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+            serializableCheck: false,
+        }),
+});
+
+// Persistor for the store
+export const persistor = persistStore(store);
