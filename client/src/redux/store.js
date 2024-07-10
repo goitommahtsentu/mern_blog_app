1d7c392a63278d1751ff030425efc61f3efc9df6
Goitom <goitommahtsentu@gmail.com>
2024-07-11 01:45:29 +0200

    complete dark mode functionality

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,11 +1,13 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import userReducer from './user/userSlice.js';
+import themeReducer from './theme/themeSlice.js';
 import { persistReducer, persistStore } from 'redux-persist';
 import storage from 'redux-persist/lib/storage'; // defaults to localStorage for web
 
 // Combine all the reducers
 const rootReducer = combineReducers({
     user: userReducer,
+    theme:themeReducer
 });
 
 // Configuration for redux-persist
