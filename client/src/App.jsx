3e528c3a0564e50081cce4a636a041bf6fa2efbf
Goitom <goitommahtsentu@gmail.com>
2024-07-12 01:01:34 +0200

    make the dashboard private

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import {BrowserRouter,Routes,Route} from "react-router-dom";
+import {BrowserRouter, Routes, Route} from "react-router-dom";
 import Home from "./pages/Home.jsx";
 import About from "./pages/About.jsx";
 import SignIn from "./pages/SignIn.jsx";
@@ -7,6 +7,7 @@ import Dashboard from "./pages/Dashboard.jsx";
 import Projects from "./pages/Projects.jsx";
 import Header from "./components/Header.jsx";
 import FooterComp from "./components/Footer.jsx";
+import PrivateRoute from "./components/PrivateRoute.jsx";
 
 const App = () => {
     return (
@@ -15,12 +16,14 @@ const App = () => {
             <Header/>
 
             <Routes>
-                <Route path="/" element={<Home/>} />
-                <Route path="/about" element={<About/>} />
-                <Route path="/sign-in" element={<SignIn/>} />
-                <Route path="/sign-up" element={<SignUp/>} />
-                <Route path="/dashboard" element={<Dashboard/>} />
-                <Route path="/projects" element={<Projects/>} />
+                <Route path="/" element={<Home/>}/>
+                <Route path="/about" element={<About/>}/>
+                <Route path="/sign-in" element={<SignIn/>}/>
+                <Route path="/sign-up" element={<SignUp/>}/>
+                <Route element={<PrivateRoute/>}>
+                    <Route path="/dashboard" element={<Dashboard/>}/>
+                </Route>
+                <Route path="/projects" element={<Projects/>}/>
             </Routes>
             <FooterComp/>
         </BrowserRouter>
