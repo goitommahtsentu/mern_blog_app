dc9401b351f08015e988edcb809208c7978c67f0
Goitom <goitommahtsentu@gmail.com>
2024-07-13 01:09:30 +0200

    complete the sidebar of the dashboard

---
+++ client/src/components/DashSidebar.jsx
@@ -0,0 +1,28 @@
+import React, {useState} from 'react';
+import {Sidebar} from "flowbite-react";
+import {HiArrowSmRight, HiUser} from "react-icons/hi";
+import {Link} from "react-router-dom";
+
+const DashSidebar = () => {
+    const [tab, setTab] = useState('');
+    return (
+        <Sidebar className='w-full md:w-56'>
+            <Sidebar.Items>
+               <Sidebar.ItemGroup>
+                   <Link to='/dashboard/?tab=profile'>
+                       <Sidebar.Item active={tab==='profile'} icon={HiUser} label='user' lblecolor='dark'>
+                           profile
+                       </Sidebar.Item>
+                   </Link>
+
+                   <Sidebar.Item icon={HiArrowSmRight} className='cursor-pointer'>
+                       Sign out
+                   </Sidebar.Item>
+               </Sidebar.ItemGroup>
+            </Sidebar.Items>
+            
+        </Sidebar>
+    );
+};
+
+export default DashSidebar;
--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import {useLocation} from "react-router-dom";
+import DashSidebar from "../components/DashSidebar.jsx";
+import DashProfile from "../components/DashProfile.jsx";
 
 const Dashboard = () => {
+    const location = useLocation();
+    const [tab, setTab] = useState('');
+
+    useEffect(() =>{
+        const urlParams = new URLSearchParams(location.search);
+        const tabFromUrl = urlParams.get('tab')
+       if(tabFromUrl){
+           setTab(tab)
+       }
+    }, [location.search])
     return (
-        <div>
-            dashboard
+        <div className='min-h-screen flex flex-col md:flex-row'>
+         <div className="md:w-56">
+             {/* sidebar*/}
+             <DashSidebar/>
+         </div>
+            {/*profile */}
+            {tab=== 'profile' && <DashProfile/>}
         </div>
     );
 };
