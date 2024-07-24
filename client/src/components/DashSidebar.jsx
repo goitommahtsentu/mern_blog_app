1f5644ccfc259ffe547b52ca4456cac37454e3d6
Goitom <goitommahtsentu@gmail.com>
2024-07-24 02:24:54 +0200

    update user profile backend and frontend

--- client/src/components/DashSidebar.jsx
+++ client/src/components/DashSidebar.jsx
@@ -10,7 +10,7 @@ const DashSidebar = () => {
             <Sidebar.Items>
                <Sidebar.ItemGroup>
                    <Link to='/dashboard/?tab=profile'>
-                       <Sidebar.Item active={tab==='profile'} icon={HiUser} label='user' lblecolor='dark'>
+                       <Sidebar.Item active={tab==='profile'} icon={HiUser} label='user' lblecolor='dark' as='div'>
                            profile
                        </Sidebar.Item>
                    </Link>
