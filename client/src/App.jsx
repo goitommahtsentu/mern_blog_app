54d975d62427b546b4f1237c03a2b4719b257e7e
Goitom <goitommahtsentu@gmail.com>
2024-08-08 22:25:05 +0200

    add create post ui

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,6 +8,8 @@ import Projects from "./pages/Projects.jsx";
 import Header from "./components/Header.jsx";
 import FooterComp from "./components/Footer.jsx";
 import PrivateRoute from "./components/PrivateRoute.jsx";
+import OnlyAdminPrivateRoute from "./components/OnlyAdminPrivateRoute.jsx";
+import CreatePost from "./pages/createPost.jsx";
 
 const App = () => {
     return (
@@ -22,6 +24,9 @@ const App = () => {
                 <Route path="/sign-up" element={<SignUp/>}/>
                 <Route element={<PrivateRoute/>}>
                     <Route path="/dashboard" element={<Dashboard/>}/>
+                </Route>
+                <Route element={<OnlyAdminPrivateRoute />}>
+                    <Route path='/create-post' element={<CreatePost />} />
                 </Route>
                 <Route path="/projects" element={<Projects/>}/>
             </Routes>
